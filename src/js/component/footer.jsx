b3e7763a38a75e7d04ae330503bb5a25ca181109
Andy Martinez <martinez1751@gmail.com>
2019-03-09 19:06:20 +0000

    added footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,7 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export class Footer extends Component {
-	render() {
-		return <h2 className="text-center">Footer</h2>;
-	}
-}
+import "../../styles/footer.scss";
+
+/**
+ *  Here I declare my component as an arrow function
+ */
+
+export const Footer = () => {
+	//loop all the items and convert them into LIs
+	const items = menu.map((item, index) => (
+		<li className="nav-item" key={index}>
+			<a className="nav-link" href={item.url}>
+				{item.label}
+			</a>
+		</li>
+	));
+
+	//this says how the footer should look like in HTML
+	return (
+		<footer className="footer">
+			<div className="container">
+				{/* here I pass the array of LIs into the UL */}
+				<ul className="nav justify-content-end">{items}</ul>
+			</div>
+		</footer>
+	);
+};
+
+// declare a JS array with the menu items that you want
+
+let menu = [
+	{ label: "Home", url: "/home" },
+	{ label: "Cart", url: "/cart" },
+	{ label: "Products", url: "/products" },
+	{ label: "Contact Us", url: "/contact" },
+	{ label: "About Us", url: "/about" }
+];
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { render } from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Logo from "../../img/logo1.jpg";
 
